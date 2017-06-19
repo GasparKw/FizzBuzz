a32a86ac089a7507955c018350fc2a4ff76880db
195868 Kwiecien Gaspar <195868@edu.p.lodz.pl>
2017-06-19 21:20:53 +0200

    Test fizzBuzz of 1 fails

--- src/test/java/code/kata/FizzBuzzTest.java
+++ src/test/java/code/kata/FizzBuzzTest.java
@@ -11,6 +11,11 @@ public class FizzBuzzTest {
 	public void fizzBuzz_of_0is0() {
 		assertThat(FizzBuzz.of(0), is("0"));
 	}
+	
+	@Test
+	public void fizzBuzz_of1is1() {
+		assertThat(FizzBuzz.of(1), is("1"));
+	}
 
 }
 
