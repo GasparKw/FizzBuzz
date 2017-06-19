631bc720340e03c9f0dd1144491ee20d72f142c4
195868 Kwiecien Gaspar <195868@edu.p.lodz.pl>
2017-06-19 21:53:55 +0200

    Main code added test passes

--- src/main/java/code/kata/FizzBuzz.java
+++ src/main/java/code/kata/FizzBuzz.java
@@ -4,9 +4,8 @@ public class FizzBuzz {
 
 	public static String of(int i) {
 		if (i == 0) return "0";
-		if (i == 5) return "Buzz";
+		if (i % 5 == 0) return "Buzz";
 		if (i % 3 == 0) return "Fizz";
 		return String.valueOf(i);
 	}
-
 }
